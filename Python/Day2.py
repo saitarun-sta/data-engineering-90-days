ba0d8414260aa39b621cc4bdba64fea9cff2e700
Sai Tarun <saitarunthota065@gmail.com>
2026-07-14 11:57:38 +0530

    Done with Day2

---
+++ Python/Day2.py
@@ -0,0 +1,26 @@
+print(20 * "=")
+
+age = input("Please enter your age: ")
+
+if int(age) >= 18:
+    print("you are eligible")
+else:
+    print("you are not eligible")
+
+
+grade = 80
+
+
+if grade > 100 :
+    print("Invalid")
+elif grade >= 80 :
+    print("A")
+elif grade < 80 and grade >= 60 :
+    print("B")
+elif grade < 60 and grade >=20:
+    print("C")
+else :
+    print("F")
+
+
+print(20 * "=")
